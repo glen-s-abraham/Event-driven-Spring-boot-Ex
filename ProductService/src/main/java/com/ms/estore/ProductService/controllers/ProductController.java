d9a004e671c41bfaf67a987b3eb201fdba0a35f4
iss211 <Glen.Abraham@immcossi.com>
2022-11-23 12:36:09 +0530

    added api gateway

--- ProductService/src/main/java/com/ms/estore/ProductService/controllers/ProductController.java
+++ ProductService/src/main/java/com/ms/estore/ProductService/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package com.ms.estore.ProductService.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+	
+	@Autowired
+	Environment env;
+	
 	@PostMapping
 	public String createProduct() {
 		return "HTTP POST Handler";
@@ -17,7 +23,7 @@ public class ProductController {
 	
 	@GetMapping
 	public String getProduct() {
-		return "HTTP GET Handler";
+		return "HTTP GET Handler "+env.getProperty("local.server.port");
 	}
 	
 	@PutMapping
